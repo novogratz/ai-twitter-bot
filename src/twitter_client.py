b28ddc416a29df0432424fb687d85fea585d3d66
novogratz <novogratz@users.noreply.github.com>
2026-04-21 23:06:46 -0400

    Add latest updates

--- src/twitter_client.py
+++ src/twitter_client.py
@@ -1,20 +1,25 @@
-import os
-import tweepy
+import subprocess
+import time
+import urllib.parse
+import webbrowser
 
 
-def get_twitter_client() -> tweepy.Client:
-    return tweepy.Client(
-        bearer_token=os.environ["TWITTER_BEARER_TOKEN"],
-        consumer_key=os.environ["TWITTER_API_KEY"],
-        consumer_secret=os.environ["TWITTER_API_SECRET"],
-        access_token=os.environ["TWITTER_ACCESS_TOKEN"],
-        access_token_secret=os.environ["TWITTER_ACCESS_TOKEN_SECRET"],
-    )
+def post_tweet(text: str):
+    """Open Twitter intent URL and auto-click Post using AppleScript."""
+    url = "https://x.com/intent/post?" + urllib.parse.urlencode({"text": text})
+    print("Opening Twitter in your browser...")
+    webbrowser.open(url)
 
+    # Wait for page to load
+    time.sleep(4)
 
-def post_tweet(text: str) -> str:
-    client = get_twitter_client()
-    response = client.create_tweet(text=text)
-    tweet_id = response.data["id"]
-    print(f"Tweet posted (id={tweet_id}): {text}")
-    return tweet_id
+    # Use AppleScript to send Cmd+Enter which posts the tweet on Twitter
+    script = '''
+    tell application "System Events"
+        keystroke return using command down
+    end tell
+    '''
+    print("Auto-clicking Post...")
+    subprocess.run(["osascript", "-e", script], check=True)
+    time.sleep(2)
+    print("Tweet posted!")
